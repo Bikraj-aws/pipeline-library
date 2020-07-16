0d9a731411a5b8e6bd2357084efc770215edeb45
Logan Donley <logan.donley@gmail.com>
2020-07-16 11:49:10 -0400

    Update imgBuildNexus.groovy

--- vars/imgBuildNexus.groovy
+++ vars/imgBuildNexus.groovy
@@ -1,5 +1,5 @@
 // vars/imgBuildNexus.groovy
-def call(String repoName, String repoOwner, String registry, Closure body) {
+def call(String imageName, String repoOwner, String registry, String imageTag = env.BUILD_NUMBER, String target = ".", String dockerFile="Dockerfile", Closure body) {
   def label = "img-${UUID.randomUUID().toString()}"
   def podYaml = libraryResource 'podtemplates/imageBuildPushNexus.yml'
   podTemplate(name: 'img', label: label, yaml: podYaml) {
@@ -7,14 +7,13 @@ def call(String repoName, String repoOwner, String registry, Closure body) {
       body()
       script {
         env.VERSION = readFile 'version.txt'
-        env.VERSION = env.VERSION.trim()
       }
-      repoName = repoName.toLowerCase()
-      repoOwner = repoOwner.toLowerCase()
+      imageNameTag()
+      gitShortCommit()
       container('img') {
         sh """
-          img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${registry}/${repoOwner}/${repoName}:${env.VERSION} ${pwd()}
-          img push ${registry}/${repoOwner}/${repoName}:${env.VERSION}
+          img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${registry}/${repoOwner}/${imageName}:${env.VERSION.trim()} ${pwd()}
+          img push ${registry}/${repoOwner}/${imageName}:${env.VERSION}
         """
       }
     }
